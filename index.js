3aa0b0318b1099d8f9048bbb159f98534de8d824
moses <moses.kamau@student.moringaschool.com>
2023-06-14 13:08:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Write your code in this file!
 const currentUser = 'Grace Hopper';
-let welcomeMessage = 'Welcome to Flatbook, ${currentUser}';
-let excitedWelcomeMessage = 'WELCOME TO FLATBOOK ${currentUser.toUpperCase} '!';
-let shortGreeting = 'welcome';
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper !';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, G !';
